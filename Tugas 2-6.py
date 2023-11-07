676357f81a0f05f26ddd1d56249a71a8aa942c64
rriezzz <146311755+rriezzz@users.noreply.github.com>
2023-11-07 23:30:29 +0700

    Add files via upload

---
+++ Tugas 2-6.py
@@ -0,0 +1,11 @@
+#total = 0
+#for i in range(1,20,2):
+#    total +=i
+ #   print(i, end=' ')
+    
+    
+    
+total = 0
+for i in range(1,20,2):
+    total +=i
+print(f'1 + 3 + 5 + 7 + 9 + 11 + 13 + 15 + 17 + 19 = {total}') 
---
+++ Tugas 3-6.py
@@ -0,0 +1,4 @@
+jumlah_baris = int(input("Masukkan jumlah baris: "))
+
+for i in range(1, jumlah_baris + 1):
+    print("*" * i)
